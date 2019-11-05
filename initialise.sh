b9d4f5dc467fe1394dda920d71890b0ba48d4835
ben <ben@surgison.net>
2019-11-05 10:09:39 +0000

    Update modules

--- initialise.sh
+++ initialise.sh
@@ -3,41 +3,41 @@
 # Helper that clones a remote Git repository, and sets up a tracking branch for
 # master.  If a develop branch exists, this is also setup and checked-out.
 # Otherwise, master is checked-out.
-function clone_repo {
-    git_host=`echo $1 | awk -F/ '{print $3}'`
-    repo_dir=${1##*/}
-    repo_dir=${repo_dir%%.git}
+function clone_repo() {
+  git_host=$(echo $1 | awk -F/ '{print $3}')
+  repo_dir=${1##*/}
+  repo_dir=${repo_dir%%.git}
 
-    if [ ! -d $repo_dir ]; then
-        echo "-- Getting ${repo_dir} from ${git_host} --"
-        git clone --branch master $1
-        cd $repo_dir || return
-        dev_branch_exists=`git ls-remote --heads $1 develop | wc -l`
-        if [ $dev_branch_exists != 0 ]; then
-            git checkout --track origin/develop
-        fi
-        cd ..
-        printf "\n\n"
+  if [ ! -d $repo_dir ]; then
+    echo "-- Getting ${repo_dir} from ${git_host} --"
+    git clone --branch master $1
+    cd $repo_dir || return
+    dev_branch_exists=$(git ls-remote --heads $1 develop | wc -l)
+    if [ $dev_branch_exists != 0 ]; then
+      git checkout --track origin/develop
     fi
+    cd ..
+    printf "\n\n"
+  fi
 
-    if [ -d $repo_dir ]; then
-        echo "-- Installing ${repo_dir} --"
-        cd $repo_dir || return
-        if [ -d node_modules/node-sass ]; then
-            if [ -d node_modules ]; then
-                npm rebuild node-sass
-            fi
-            npm install
-            npm audit fix
-            npm update
-            npm install
-        else
-             npm install
-             npm shrinkwrap
-        fi
-        cd ..
-        printf "\n\n"
+  if [ -d $repo_dir ]; then
+    echo "-- Installing ${repo_dir} --"
+    cd $repo_dir || return
+    if [ -d node_modules/node-sass ]; then
+      if [ -d node_modules ]; then
+        npm rebuild node-sass
+      fi
+      npm install
+      npm audit fix
+      npm update
+      npm install
+    else
+      npm install
+      npm shrinkwrap
     fi
+    cd ..
+    printf "\n\n"
+  fi
 }
 
 # Get all the repos required for Waste Exemptions.
