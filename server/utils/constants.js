1a56461debfc80cd886989859a665b26cb15f3b7
Duncan Nixon <duncannixon@users.noreply.github.com>
2021-04-20 11:09:01 +0100

    Session-specific Redis storage (#29)
    IVORY-306: Make data stored in Redis session-specific
    
    Added creation of a session cookie to the home page.
    
    Generated a unique session ID and added to the session cookie.
    
    Appended session ID to Redis keys using the following format: sessionId.redisKey
    
    e.g. 91bde99f-c98e-4ff7-8d87-b786b8fb6eb4.ivory-added
    
    Created a RedisService responsible for getting and setting Redis valus and setting expiry (Time To Live / TTL) on values stored Redis. Currently hard-coded to 86400 (i.e. one day in seconds). This could be made a configurable parameter if required.
    
    Also, added novalidate to forms to turn off HTML5 validation as per GDS guidelines.
    
    Unit tests to follow later as the framework is under development on a separate branch.

--- server/utils/constants.js
+++ server/utils/constants.js
@@ -15,3 +15,11 @@ Constants.Views = {
   TAKEN_FROM_ELEPHANT: 'taken-from-elephant',
   YES_NO_IDK: 'yes-no-idk'
 }
+
+Constants.RedisKeys = {
+  IVORY_ADDED: 'ivory-added',
+  IVORY_INTEGRAL: 'ivory-integral'
+}
+
+Constants.DEFRA_IVORY_SESSION_KEY = 'DefraIvorySession'
+Constants.SESSION_ID = 'sessionId'
