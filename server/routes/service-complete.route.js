6ef529b726ce7601803acf9407bb2df6b11dbafc
Jason Claxton <30830544+Jozzey@users.noreply.github.com>
2021-08-24 11:24:48 +0100

    Feature/ivory 396 confirmation page (#111)
    * updated packages
    
    * Updated page content
    
    * created unit tests
    
    * Fixed vulnerability in node_modules/tar

--- server/routes/service-complete.route.js
+++ server/routes/service-complete.route.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { Paths, RedisKeys, Views, PaymentResult } = require('../utils/constants')
+const { Paths, RedisKeys, Views, PaymentResult, ItemType } = require('../utils/constants')
 const PaymentService = require('../services/payment.service')
 const RedisService = require('../services/redis.service')
 
@@ -29,14 +29,48 @@ const handlers = {
 }
 
 const _getContext = async request => {
+  const itemType = await RedisService.get(
+    request,
+    RedisKeys.WHAT_TYPE_OF_ITEM_IS_IT
+  )
+
+  const isSection2 = itemType === ItemType.HIGH_VALUE
+
   const submissionReference = await RedisService.get(
     request,
     RedisKeys.SUBMISSION_REFERENCE
   )
 
+  const applicantEmail = await RedisService.get(
+    request,
+    RedisKeys.APPLICANT_EMAIL_ADDRESS
+  )
+
+  const ownerEmail = await RedisService.get(
+    request,
+    RedisKeys.OWNER_EMAIL_ADDRESS
+  )
+
   return {
-    pageTitle: 'Service complete',
+    pageTitle: isSection2
+      ? 'Application received'
+      : 'Self-assessment complete',
     submissionReference,
+    helpText1: isSection2
+      ? 'We’ve sent confirmation of this application to:'
+      : 'We’ve also sent these details to:',
+    applicantEmail,
+    ownerEmail,
+    helpText2: isSection2
+      ? 'An expert will now check your application.'
+      : 'You can sell or hire out the item at your own risk.',
+    helpText3: isSection2
+      ? 'Checks usually happen within 30 days, and we may contact you during this time if we require more information.'
+      : 'If you do so, and we later discover that you’ve given us false information, you could be fined or prosecuted.',
+    helpText4: isSection2
+      ? 'If your application is approved, we will send you an exemption certificate so you can sell or hire out your item.'
+      : 'This self-assessment lasts until the owner of the item changes.',
+    helpText5: isSection2,
     hideBackLink: true
   }
 }
